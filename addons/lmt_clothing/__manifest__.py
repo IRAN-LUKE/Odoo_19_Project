d5e6129a6a706d11ddf5743054e781003c3e4a96
Lwin Minn Thant <lwinmin@innovix-solutions.com>
2026-04-30 10:52:22 +0630

    [ADD]Initial stage of project

---
+++ addons/lmt_clothing/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'LMT Clothing',
+    'category': 'custom',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'Lwin Minn Thant',
+    'license': 'LGPL-3',
+
+'auto_install': False,
+}
